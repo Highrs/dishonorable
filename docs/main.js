2a5355976f944b417dffb64fb3826940bdd5f9ad
Highscore <gchapyzh@gmail.com>
2023-11-16 23:02:13 -0800

    test 2

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,20 @@
 'use strict';
 
+import { io } from "socket.io-client";
+
+const socket = io("ws://localhost:3000");
+
+
+
 const main = async () => {
     console.log('Giant alien spiders are no joke!');
+
+    socket.on("hello", (arg) => {
+        console.log(arg);
+    });
+
+    socket.emit("howdy", "stranger");
+
 };
 
 window.onload = main;
