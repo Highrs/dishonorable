f06e20369eff4feff36860f2ec53f2350301dc4a
gchapyzh <gchapyzh@gmail.com>
2023-11-20 23:14:42 -0800

    Canvas installation

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 'use strict';
 
-const socket = new WebSocket("ws://localhost:8080");
+const getSvg = require('./get-svg.js');
+const renderer = require('onml/renderer.js');
 
-// Connection opened
-socket.addEventListener("open", (event) => {
-    let mail = "Hello Server!";
-    console.log('sending ' + mail)
-    socket.send(mail);
-});
+function getPageWidth() {return document.body.clientWidth;}
+function getPageHeight() {return document.body.clientHeight;}
 
-// Listen for messages
-socket.addEventListener("message", (event) => {
-    console.log("Message from server: ", event.data);
-});
+const mkRndr = (place) => {
+    return renderer(document.getElementById(place));
+};
+
+const drawStatic = () => {
+    return getSvg({w:getPageWidth(), h:getPageHeight(), i:'allTheStuff'}).concat([
+      ['g', {id: 'mainCanvas'}]
+    ]);
+  };
 
 const main = async () => {
     console.log('Giant alien spiders are no joke!');
+    
+    let renderStatic = mkRndr('content');
+    renderStatic(drawStatic());
+
+    const socket = new WebSocket("ws://localhost:8080");
+
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+        console.log('Connected!');
+        // console.log(socket);
+        // console.log(event);
+    });
+
+    // Listen for messages
+    socket.addEventListener("message", (event) => {
+        console.log("Server says: ", event.data);
+    });
 };
 
 window.onload = main;
